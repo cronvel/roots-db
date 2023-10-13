c2bf5056090addc52a929750bb36a3ca5982a737
Cédric Ronvel <cedric.ronvel@gmail.com>
2023-10-13 17:29:30 +0200

    Very basic import support

--- test/roots-db-test.js
+++ test/roots-db-test.js
@@ -8718,7 +8718,22 @@ if ( IMPORTER ) {
 			
 			var batch = await jobs.collect( {} ) ;
 
-			log.info( "Jobs: %I" , [ ... batch ] ) ;
+			//log.info( "Jobs: %I" , [ ... batch ] ) ;
+
+			// MongoDB may shuffle things up, so we don't use an array here
+			var map = {} ;
+			batch.forEach( doc => map[ doc.title ] = doc ) ;
+
+			expect( map ).to.only.have.own.keys( 'dev' , 'devops' ) ;
+			expect( map ).to.partially.equal( {
+				dev: {
+					_id: map.dev.getId() , title: 'dev' , salary: 3500
+				} ,
+				devops: {
+					_id: map.devops.getId() , title: 'devops' , salary: 3200
+				}
+			} ) ;
+			
 		} ) ;
 	} ) ;
 }
